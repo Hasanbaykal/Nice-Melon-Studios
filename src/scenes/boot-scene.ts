c73316fada01588d156b417e2d6834069773ff9e
Ivar Toussaint <ivar.toussaint@gmail.com>
2019-05-20 13:41:43 +0200

    made worm smaller

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -11,13 +11,9 @@ export class BootScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.image('sky', require('../assets/background.png'))
         this.load.image('boot', require('../assets/bg.png'))
-        this.load.image('fancy', require('../assets/fancybackground.png'))
         this.load.image('star', require('../assets/star.png'))
         this.load.image('bomb', require('../assets/bomb.png'))
-        this.load.image('ice', require('../assets/platform_ice.png'))
-        this.load.image('platform', require('../assets/platform_grass.png'))
         this.load.image('ground', require('../assets/platform_ground.png'))
         this.load.image('worm', require('../assets/worm.png'))
         this.load.image('enemy', require('../assets/enemy.png'))
