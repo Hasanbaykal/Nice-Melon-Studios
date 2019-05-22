8c11616886014e0ae56aad4b151a6886ffd060df
Ivar Toussaint <ivar.toussaint@gmail.com>
2019-05-22 10:43:06 +0200

    added joystick utils

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -1,9 +1,11 @@
 import { GameScene } from "../scenes/game-scene"
+import { Joystick } from "../utils/joystick"
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
 
     private cursors: Phaser.Input.Keyboard.CursorKeys
     private GameScene: GameScene
+    private joystick : Joystick
 
     constructor(scene: GameScene) {
         super(scene, 100, 450, "worm")
@@ -14,6 +16,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         this.scene.add.existing(this)
         this.addParticles()
         this.scene.physics.add.existing(this)
+        this.joystick = new Joystick(6)
 
         this.setCollideWorldBounds(true)
         this.setDrag(1000)
@@ -21,11 +24,19 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     }
 
     public update(){
+        this.joystick.update()
+        this.joystickInput()
         this.keyController()
+        
     }
     private handleFireButton():void {
         this.GameScene.friendlyBullet()
 
+    }
+
+    private joystickInput():void {
+        this.setVelocityX(this.joystick.XAxis * 400)
+        this.setVelocityY(this.joystick.YAxis * 400)
     }
 
     private addParticles() {
@@ -47,16 +58,16 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     public keyController() {
         if (this.cursors.left.isDown) {
-            this.setVelocityX(-250)
+            this.setVelocityX(-200)
             this.flipX = false
         } if (this.cursors.right.isDown) {
-            this.setVelocityX(250)
+            this.setVelocityX(200)
             this.flipX = false
         } if (this.cursors.up.isDown) {
-            this.setVelocityY(-250)
+            this.setVelocityY(-200)
             this.flipX = false
         } if (this.cursors.down.isDown) {
-            this.setVelocityY(250)
+            this.setVelocityY(200)
             this.flipX = false
         }
         if(this.scene.input.keyboard.checkDown(this.cursors.space, 500)){
